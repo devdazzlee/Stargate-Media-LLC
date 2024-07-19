9aee07e75a51ace56d1902201dcb0f8c790e3264
Ahmed Raza <bhaia9036@gmail.com>
2024-07-19 06:19:36 -0200

    Updated

--- src/Components/PaymentForm/PaymentForm.jsx
+++ src/Components/PaymentForm/PaymentForm.jsx
@@ -105,7 +105,7 @@ const PaymentForm1 = () => {
               name="firstName"
               value={deliveryDetails.firstName}
               onChange={handleDeliveryChange}
-              placeholder="First name (optional)"
+              placeholder="First name "
               className="border border-gray-300 rounded-lg px-3 py-2 w-full focus:ring-2 focus:ring-indigo-600 focus:outline-none"
             />
             <input
